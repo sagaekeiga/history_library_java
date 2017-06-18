5324b3fa598ad3afd09b52bb1e5084a7be1d8fa7
寒河江京我 <keiga@sagaemiyakowarenoMacBook-Pro.local>
2017-06-18 22:33:19 +0900

    first

--- app/src/main/java/history/historylibrary/MainActivity.java
+++ app/src/main/java/history/historylibrary/MainActivity.java
@@ -2,10 +2,15 @@ package history.historylibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -16,6 +21,9 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+
+
+import static okhttp3.Protocol.get;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -37,7 +45,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         textViewRes = (TextView) findViewById(R.id.tv_res);
-        textViewDes = (TextView) findViewById(R.id.tv_des);
 
         buttonPostTest = (Button) findViewById(R.id.btn_post_test);
         buttonPostTest.setOnClickListener(this);
@@ -74,11 +81,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                res = response.body().string();
+                  res = response.body().string();
                 runOnUiThread(new Runnable() {
                     public void run() {
+                        System.out.println(res);
+                        try {
+                            JSONArray Jarray = new JSONArray(res);
+                            System.out.println("成功");
+                            for (int i = 0; i < Jarray.length(); i++) {
+                                JSONObject object     = Jarray.getJSONObject(i);
+                                System.out.println(object.getString("id"));
+                            }
+                        } catch (JSONException e) {
+                            System.out.println("失敗");
+                            System.out.println(e);
+                        }
                         textViewRes.setText(res);
-                        textViewDes.setText("No Data");
                     }
                 });
             }
